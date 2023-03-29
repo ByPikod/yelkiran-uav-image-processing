3c6067d11f273d495f46f0a272e241398e6c4a0b
ByPikod <yahyabatulu00@gmail.com>
2023-03-29 15:34:33 +0300

    mass update

--- src/processor.py
+++ src/processor.py
@@ -19,6 +19,12 @@ class Processor:
     bindings: Bindings
     config: conf.ConfigUtil
 
+    preview: bool
+    record: bool
+
+    result: cv2.VideoWriter
+    capture: cv2.VideoCapture
+
     def __init__(self):
 
         # Configuration
@@ -42,49 +48,55 @@ class Processor:
             self.binding = Server(config.get_string("simulator.host"), config.get_int("simulator.port"))
 
         self.config = config
+        self.preview = self.config.get_bool("general.preview")
+        self.record = self.config.get_bool("general.record")
 
         try:
-            self.process()
+            self.start_loop()
         except cv2.error as err:
             print(f"An error has occurred while processing video camera: \n{err}")
 
-    def process(self):
+    def get_capture_size(self) -> tuple[int, int]:
+        """Returns the size of the capture."""
+        frame_width = int(self.capture.get(3))
+        frame_height = int(self.capture.get(4))
+        return frame_width, frame_height
+
+    def start_loop(self):
         """Main loop for image processing."""
 
-        vid = cv2.VideoCapture(self.config.get_int("general.camera-index"))
+        self.capture = cv2.VideoCapture(self.config.get_int("general.camera-index"))
 
         if self.config.get_bool("simulator.enabled"):
-            vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-            vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+            self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+            self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-        # Get size
-        frame_width = int(vid.get(3))
-        frame_height = int(vid.get(4))
-        size = (frame_width, frame_height)
-
-        # Create recording
+        # Create recording if enabled.
         video_path = os.path.join(self.record_dir, "video.avi")
-        result = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'XVID'), 30, size)
+        if self.record:
+            size = self.get_capture_size()
+            self.result = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'XVID'), 30, size)
 
-        preview = self.config.get_bool("general.preview")
-        record = self.config.get_bool("general.record")
-
+        # Start the loop
         while True:
-
-            ret, frame = vid.read()
-
-            if record:
-                result.write(frame)  # Save video
-
-            if preview:
-                cv2.imshow('Recording', frame)  # Show video
-
-            if cv2.waitKey(5) & 0xFF == ord('q'):
+            ret, frame = self.capture.read()
+            if not self.process(frame):
                 break
 
         # After the loop release the cap object
-        vid.release()
-        result.release()
-
+        self.capture.release()
         # Destroy all the windows
         cv2.destroyAllWindows()
+
+    def process(self, frame):
+
+        if self.record:
+            self.result.write(frame)  # Save video
+
+        if self.preview:
+            cv2.imshow('Recording', frame)  # Show video
+
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            return False
+
+        return True
